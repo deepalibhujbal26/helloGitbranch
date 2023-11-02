4734a3a5def469ce8473019891b19716092e74a7
deepalibhujbal26 <deepalibhujbal26@gmail.com>
2023-11-01 22:57:18 -0400

    byebye class added

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -2,5 +2,10 @@ public class main {
     public static void main(String[] args) {
         hello a1 = new hello();
         a1.a = 10;
+
+
+        byebye b1=new byebye();
+        b1.bye=10;
     }
+
 }
